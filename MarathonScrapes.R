b2b1d7aecacc4f86faa6f26dbb20364363159ae2
Greg DeVore <31558541+gregdevore@users.noreply.github.com>
2018-03-18 07:33:01 -0700

    Added comments and cleaned up logic

--- MarathonScrapes.R
+++ MarathonScrapes.R
@@ -7,45 +7,46 @@ library(dplyr)
 library(tidyr)
 library(ggplot2)
 
+# Start selenium server and browser
 rsDriver()
-remDr <- remoteDriver(remoteServerAddr = "localhost" 
-                      , port = 4445L
-                      , browserName = "chrome")
+remDr <- remoteDriver(remoteServerAddr = "localhost", port = 4445L, browserName = "chrome")
 remDr$open()
-
+# Start at base URL
 url <- 'http://www.marathonguide.com/results/browse.cfm?MIDD=472171105'
 p <- html_session(url)
-
+# Store yearly results in list (may have different number of columns depending on the year)
 overall_results <- list()
 i <- 1
-gender <- 'M'
+gender <- 'Men'
 years <- c(seq(2000,2011),seq(2013,2017))
 for (year in years) {
-    cat('Scraping year',year,'for',gender,'results\n')
+    cat('Scraping year ',year,' for ',gender,'\'s results\n', sep = '')
     pi <- follow_link(p,as.character(year))
     # Navigate to page
     remDr$navigate(pi$url)
     
-    # Select results and select top 100 male or female finishers
-    if (gender == 'M') {
-      raceRange <- remDr$findElement(using = 'css selector', 'select+ p select')      
+    # Select proper results dropdown and top 100 finishers
+    if (gender == 'Men') {
+      css <- 'select+ p select'
     } else {
-      raceRange <- remDr$findElement(using = 'css selector', 'p+ p select')      
+      css <- 'p+ p select'
     }
+    raceRange <- remDr$findElement(using = 'css selector', css)      
     raceRange$clickElement()
-    if (gender == 'M') {
-      raceRangeValue <- remDr$findElement(using = 'xpath', '/html/body/table[2]/tbody/tr[1]/td[2]/table[3]/tbody/tr[2]/td/table/tbody/tr/td/table/tbody/tr/td/table[2]/tbody/tr/td[1]/form/p[1]/select/option[2]')      
+    if (gender == 'Men') {
+      xpath <- '/html/body/table[2]/tbody/tr[1]/td[2]/table[3]/tbody/tr[2]/td/table/tbody/tr/td/table/tbody/tr/td/table[2]/tbody/tr/td[1]/form/p[1]/select/option[2]'
     } else {
-      raceRangeValue <- remDr$findElement(using = 'xpath', '/html/body/table[2]/tbody/tr[1]/td[2]/table[3]/tbody/tr[2]/td/table/tbody/tr/td/table/tbody/tr/td/table[2]/tbody/tr/td[1]/form/p[2]/select/option[2]')            
+      xpath <- '/html/body/table[2]/tbody/tr[1]/td[2]/table[3]/tbody/tr[2]/td/table/tbody/tr/td/table/tbody/tr/td/table[2]/tbody/tr/td[1]/form/p[2]/select/option[2]'
     }
+    raceRangeValue <- remDr$findElement(using = 'xpath', xpath)
     raceRangeValue$clickElement()
     # Click 'View' to see race results
     viewButton <- remDr$findElement(using = 'xpath', '/html/body/table[2]/tbody/tr[1]/td[2]/table[3]/tbody/tr[2]/td/table/tbody/tr/td/table/tbody/tr/td/table[2]/tbody/tr/td[1]/form/p[3]/input[3]')
     viewButton$clickElement()
     # Give page time to load
-    Sys.sleep(5)
+    Sys.sleep(2)
     # Grab HTML table and parse into data frame
-    table <- remDr$findElement(using = 'xpath', '/html/body/table[2]/tbody/tr[1]/td[2]/table[3]/tbody/tr[2]/td/table')
+    table <- remDr$findElement(using = 'xpath','/html/body/table[2]/tbody/tr[1]/td[2]/table[3]/tbody/tr[2]/td/table')
     elemtxt <- table$getElementAttribute('outerHTML')
     elemxml <- htmlTreeParse(elemtxt[[1]], useInternalNodes=T)
     results <- readHTMLTable(elemxml)[[1]]
@@ -81,20 +82,21 @@ results.df <- results.df %>% mutate(TotalTime = Hour + Minute/60 + Second/3600)
 results.timeSummary <- results.df %>% group_by(Year) %>% 
   summarize(bestTime = min(TotalTime), worstTime = max(TotalTime), meanTime = mean(TotalTime),
             sd = sd(TotalTime))
-
+# Plot best times
 ggplot(results.timeSummary, aes(x = Year, y = bestTime)) + geom_line() + 
-  ylim(2.0,2.5) + ylab('Time (Hours)') + xlab('Year') + ggtitle('Men\'s Best Finishing Time')
-
+  ylim(2.0,2.5) + ylab('Time (Hours)') + xlab('Year') + ggtitle(sprintf('%s\'s Best Finishing Time',gender))
+# Plot average time with error bars (1 SD)
 ggplot(results.timeSummary, aes(x = Year, y = meanTime)) +
   geom_ribbon(aes(ymin = meanTime - sd, ymax = meanTime + sd), fill = "grey70") + 
-  geom_line() + ylab('Time (Hours)') + xlab('Year') + ggtitle('Men\'s Mean Finishing Time With SD Bars') +
+  geom_line() + ylab('Time (Hours)') + xlab('Year') + 
+  ggtitle(sprintf('%s\'s Mean Finishing Time With SD Bars',gender)) +
   ylim(2,3.5)
-
+# Plot distribution of finishing times for each year
 minT <- min(results.df$TotalTime)
 maxT <- max(results.df$TotalTime)
-
 ggplot(results.df %>% filter(Year < 2004), aes(TotalTime)) + geom_density() + 
-  facet_grid(Year ~ .) + xlim(minT, maxT) + ylim(0, 6) + ggtitle('Men\'s Finishing Times By Year')
+  facet_grid(Year ~ .) + xlim(minT, maxT) + ylim(0, 6) + 
+  ggtitle(sprintf('%s\'s Finishing Times By Year',gender))
 ggplot(results.df %>% filter(Year >= 2004 & Year < 2008), aes(TotalTime)) + geom_density() + 
   facet_grid(Year ~ .) + xlim(minT, maxT) + ylim(0, 6)
 ggplot(results.df %>% filter(Year >= 2008 & Year < 2012), aes(TotalTime)) + geom_density() + 
